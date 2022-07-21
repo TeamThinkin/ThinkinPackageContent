9fd23b6bc41c450c31c42664c62ffc7ef8d3e076
greengiant83 <greengiant83@hotmail.com>
2022-07-21 11:15:59 -0500

    Added asset bundle labels and spawn points

--- Assets/Scripts/Editor/AutoBuild.cs
+++ Assets/Scripts/Editor/AutoBuild.cs
@@ -7,24 +7,39 @@ using UnityEngine;
 
 public class AutoBuild : MonoBehaviour
 {
-    [MenuItem("Thinkin/Build Addressables")]
-    public static void ExecuteBuild()
+    //[MenuItem("Thinkin/Build Addressables")]
+    //public static void ExecuteBuild()
+    //{
+    //    Debug.Log("Building addressables packages");
+
+    //    Debug.Log("Switching build platform to Android");
+    //    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
+    //    Debug.Log("Building Android package");
+    //    AddressableAssetSettings.BuildPlayerContent();
+
+    //    Debug.Log("Switching build platform to Windows (x86)");
+    //    EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
+    //    Debug.Log("Building Windows package");
+    //    AddressableAssetSettings.BuildPlayerContent();
+
+    //    Debug.Log("Addressables Build Complete");
+
+    //    OpenUploadWindows();
+    //}
+
+
+    [MenuItem("Thinkin/Build AssetBundles")]
+    static void BuildAllAssetBundles()
     {
-        Debug.Log("Building addressables packages");
-        
-        Debug.Log("Switching build platform to Android");
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
-        Debug.Log("Building Android package");
-        AddressableAssetSettings.BuildPlayerContent();
+        string basePath = "D:\\Temp\\Assets";
+        string androidPath = basePath + "\\Android";
+        string win64Path = basePath + "\\Windows64";
 
-        Debug.Log("Switching build platform to Windows (x86)");
-        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows);
-        Debug.Log("Building Windows package");
-        AddressableAssetSettings.BuildPlayerContent();
+        if (!Directory.Exists(androidPath)) Directory.CreateDirectory(androidPath);
+        if (!Directory.Exists(win64Path)) Directory.CreateDirectory(win64Path);
 
-        Debug.Log("Addressables Build Complete");
-
-        OpenUploadWindows();
+        BuildPipeline.BuildAssetBundles(androidPath, BuildAssetBundleOptions.None, BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles(win64Path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
     }
 
     [MenuItem("Thinkin/Open Upload Windows")]
@@ -35,20 +50,4 @@ public class AutoBuild : MonoBehaviour
         System.Diagnostics.Process.Start(@"c:\windows\explorer.exe", path);
     }
 
-    public class CreateAssetBundles
-    {
-        [MenuItem("Thinkin/Build AssetBundles")]
-        static void BuildAllAssetBundles()
-        {
-            string basePath = "D:\\Temp\\Assets";
-            string androidPath = basePath + "\\Android";
-            string win64Path = basePath + "\\Windows64";
-
-            if (!Directory.Exists(androidPath)) Directory.CreateDirectory(androidPath);
-            if (!Directory.Exists(win64Path)) Directory.CreateDirectory(win64Path);
-
-            BuildPipeline.BuildAssetBundles(win64Path, BuildAssetBundleOptions.None, BuildTarget.Android);
-            BuildPipeline.BuildAssetBundles(win64Path, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
-        }
-    }
 }
